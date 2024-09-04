b0e1b9950a213b8392c6a4c72e05d1380020e694
Hannes Lau <office@hanneslau.de>
2024-09-04 15:30:53 +0200

    [TASK] Update to PTI 5

--- app/src/pti_demo/Classes/ExtbaseViewResolver.php
+++ app/src/pti_demo/Classes/ExtbaseViewResolver.php
@@ -8,8 +8,11 @@ use PrototypeIntegration\PrototypeIntegration\Processor\PtiDataProcessor;
 use PrototypeIntegration\PrototypeIntegration\View\ViewAdapter;
 use PrototypeIntegration\PrototypeIntegration\View\ViewResolverInterface;
 use Psr\Container\ContainerInterface;
+use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
 use TYPO3\CMS\Extbase\Mvc\View\GenericViewResolver;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3Fluid\Fluid\View\ViewInterface;
 
 class ExtbaseViewResolver extends GenericViewResolver
@@ -42,6 +45,18 @@ class ExtbaseViewResolver extends GenericViewResolver
                 fn(string $className): PtiDataProcessor => GeneralUtility::makeInstance($className),
                 $override['processors'],
             );
+
+            // Allow the CompoundProcessor to force json output
+            $configurationManager = GeneralUtility::makeInstance(ConfigurationManager::class);
+            $contentObject = $configurationManager->getContentObject();
+            $ptiForceView = $contentObject?->data['_pti_force_view'] ?? null;
+            if (
+                // USER_INT objects have to be rendered with their real template on the second pass, when replacing the USER_INT string
+                $contentObject->getUserObjectType() === ContentObjectRenderer::OBJECTTYPE_USER
+                && isset($ptiForceView)
+            ) {
+                $format = $ptiForceView;
+            }
 
             $view = $this->viewResolver->getViewForExtbaseAction(
                 $controllerObjectName,
